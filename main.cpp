8f0a19aa1727ee08e77bc67339715edb4a7e0347
HuangZhen <1412690197@qq.com>
2023-11-16 01:41:25 +0800

    simple version without line search

--- main.cpp
+++ main.cpp
@@ -146,9 +146,22 @@ public:
   using VectorU = Matrix<UDim, 1>;
   using MatrixXX = Matrix<XDim, XDim>;
   using MatrixXU = Matrix<XDim, UDim>;
-  virtual ~DynamicModel();
-  virtual VectorX ComputeNextState(const VectorX &x, const VectorU &u);
-  virtual std::pair<MatrixXX, MatrixXU> ComputeDerivatives(const VectorX &x, const VectorU &u);
+  virtual ~DynamicModel() = default;
+  virtual VectorX ComputeNextState(const VectorX &x, const VectorU &u) const;
+  virtual std::pair<MatrixXX, MatrixXU> ComputeDerivatives(const VectorX &x, const VectorU &u) const;
+};
+
+class MyDynamicModel : public DynamicModel<2,2> {
+public:
+  ~MyDynamicModel() override = default;
+  VectorX ComputeNextState(const VectorX &x, const VectorU &u) const override {
+    return x + u;
+  }
+  std::pair<MatrixXX, MatrixXU> ComputeDerivatives(const VectorX &, const VectorU &) const override {
+    MatrixXX fx;
+    fx[0][0] = fx[1][1] = 1;
+    return std::make_pair(fx, fx);
+  }
 };
 
 template <int XDim, int UDim>
@@ -159,7 +172,7 @@ public:
   using MatrixXX = Matrix<XDim, XDim>;
   using MatrixUX = Matrix<UDim, XDim>;
   using MatrixUU = Matrix<UDim, UDim>;
-  virtual ~TransitionCost();
+  virtual ~TransitionCost() = default;
   struct Derivatives {
     VectorX df_dx;
     VectorU df_du;
@@ -167,7 +180,23 @@ public:
     MatrixUX d2f_du_dx;
     MatrixUU d2f_du_du;
   };
-  virtual double ComputeCostAndDerivatives(const VectorX &x, const VectorU &u, Derivatives *derivatives);
+  virtual double ComputeCostAndDerivatives(const VectorX &x, const VectorU &u, Derivatives *derivatives) const;
+};
+
+class MyTransitionCost : public TransitionCost<2,2> {
+public:
+  ~MyTransitionCost() override = default;
+  double ComputeCostAndDerivatives(const VectorX &, const VectorU &u, Derivatives *derivatives) const override {
+    double cost = (u.transpose() * u)[0][0];
+    if (derivatives) {
+      *derivatives = Derivatives();
+      derivatives->df_du[0][0] = u[0][0] * 2.0;
+      derivatives->df_du[1][0] = u[1][0] * 2.0;
+      derivatives->d2f_du_du[0][0] = 2.0;
+      derivatives->d2f_du_du[1][1] = 2.0;
+    }
+    return cost;
+  }
 };
 
 template <int XDim>
@@ -175,12 +204,28 @@ class StateCost {
 public:
   using VectorX = Matrix<XDim, 1>;
   using MatrixXX = Matrix<XDim, XDim>;
-  virtual ~StateCost();
+  virtual ~StateCost() = default;
   struct Derivatives {
     VectorX df_dx;
     MatrixXX d2f_dx_dx;
   };
-  virtual double ComputeCostAndDerivatives(const VectorX &x, Derivatives *derivatives);
+  virtual double ComputeCostAndDerivatives(const VectorX &x, Derivatives *derivatives) const;
+};
+
+class MyFinalCost : public StateCost<2> {
+public:
+  ~MyFinalCost() override = default;
+  double ComputeCostAndDerivatives(const VectorX &x, Derivatives *derivatives) const override {
+    double cost = (x[0][0] - 100) * (x[0][0] - 100) + (x[1][0] - 100) * (x[1][0] - 100);
+    if (derivatives) {
+      *derivatives = Derivatives();
+      derivatives->df_dx[0][0] = 2.0 * (x[0][0] - 100);
+      derivatives->df_dx[1][0] = 2.0 * (x[1][0] - 100);
+      derivatives->d2f_dx_dx[0][0] = 2.0;
+      derivatives->d2f_dx_dx[1][1] = 2.0;
+    }
+    return cost;
+  }
 };
 
 template <int XDim, int UDim>
@@ -192,28 +237,31 @@ public:
   using MatrixUX = Matrix<UDim, XDim>;
   using MatrixUU = Matrix<UDim, UDim>;
   using MatrixXU = Matrix<XDim, UDim>;
+  using DynamicModel = DynamicModel<XDim, UDim>;
   using TransitionCost = TransitionCost<XDim, UDim>;
   using StateCost = StateCost<XDim>;
   using Q = TransitionCost::Derivatives;
   using V = StateCost::Derivatives;
 
-  static std::optional<V> ComputeV(const Q &q) {
+  static bool ComputeKV(const Q &q, VectorU *ku, MatrixUX *kux, V *v) {
+    assert(ku != nullptr);
+    assert(kux != nullptr);
+    assert(v != nullptr);
     std::optional<MatrixUU> l = q.d2f_du_du.ComputeLLT();
     if (l == std::nullopt) {
-      return std::nullopt;
+      return false;
     }
-    VectorU ku = SolveUsingLLT(*l, q.df_du) * -1.0;
-    MatrixUX kux = SolveUsingLLT(*l, q.d2f_du_dx) * -1.0;
-    return V {
-      .df_dx = q.df_dx +
-               kux.transpose() * q.d2f_du_du * ku +
-               kux.transpose() * q.df_du +
-               q.d2f_du_dx.transpose() * ku,
-      .d2f_dx_dx = q.d2f_dx_dx +
-                   kux.transpose() * q.d2f_du_du * kux +
-                   kux.transpose() * q.d2f_du_dx +
-                   q.d2f_du_dx.transpose() * kux,
-    };
+    *ku = SolveUsingLLT(*l, q.df_du) * -1.0;
+    *kux = SolveUsingLLT(*l, q.d2f_du_dx) * -1.0;
+    v->df_dx = q.df_dx +
+               kux->transpose() * q.d2f_du_du * *ku +
+               kux->transpose() * q.df_du +
+               q.d2f_du_dx.transpose() * *ku;
+    v->d2f_dx_dx = q.d2f_dx_dx +
+                   kux->transpose() * q.d2f_du_du * *kux +
+                   kux->transpose() * q.d2f_du_dx +
+                   q.d2f_du_dx.transpose() * *kux;
+    return true;
   }
   
   static Q ComputeQ(const V &v, const MatrixXX &fx, const MatrixXU &fu) {
@@ -225,14 +273,77 @@ public:
       .d2f_du_du = fu.transpose() * v.d2f_dx_dx * fu,
     };
   }
+  
+  void Solve() {
+    assert(x_.size() == 1);
+    assert(u_.size() >= 1);
+    assert(dynamic_model_ != nullptr);
+    assert(transition_cost_ != nullptr);
+    assert(final_cost_ != nullptr);
+    for (const auto& u : u_) {
+      x_.push_back(dynamic_model_->ComputeNextState(x_.back(), u));
+    }
+    int n = u_.size();
+    q_.resize(n);
+    v_.resize(n + 1);
+    ku_.resize(n);
+    kux_.resize(n);
+    for (int iter = 0; iter < 5; ++iter) {
+      final_cost_->ComputeCostAndDerivatives(x_[n], &v_[n]);
+      for (int i = n - 1; i >= 0; --i) {
+        std::pair<MatrixXX, MatrixXU> fx_and_fu = dynamic_model_->ComputeDerivatives(x_[i], u_[i]);
+        q_[i] = ComputeQ(v_[i + 1], fx_and_fu.first, fx_and_fu.second);
+        Q derivatives;
+        transition_cost_->ComputeCostAndDerivatives(x_[i], u_[i], &derivatives);
+        q_[i].df_dx += derivatives.df_dx;
+        q_[i].df_du += derivatives.df_du;
+        q_[i].d2f_dx_dx += derivatives.d2f_dx_dx;
+        q_[i].d2f_du_dx += derivatives.d2f_du_dx;
+        q_[i].d2f_du_du += derivatives.d2f_du_du;
+        
+        if (!ComputeKV(q_[i], &ku_[i], &kux_[i], &v_[i])) {
+          printf("end at iter = %d\n", iter);
+          return;
+        }
+      }
+      
+      std::vector<VectorX> last_x = x_;
+      
+      for (int i = 0; i < n; ++i) {
+        u_[i] += ku_[i] + kux_[i] * (x_[i] - last_x[i]);
+        x_[i + 1] = dynamic_model_->ComputeNextState(x_[i], u_[i]);
+      }
+      
+      printf("iter = %d\n", iter);
+      for (int i = 0; i < (int)x_.size(); ++i) {
+        printf("x_[%d]\n", i);
+        x_[i].print();
+      }
+      for (int i = 0; i < (int)u_.size(); ++i) {
+        printf("u_[%d]\n", i);
+        u_[i].print();
+      }
+      for (int i = 0; i < (int)v_.size(); ++i) {
+        printf("vx %d\n", i);
+        v_[i].df_dx.print();
+        printf("vxx %d\n", i);
+        v_[i].d2f_dx_dx.print();
+      }
+    }
+  }
 
-private:
-  int n_;
+// private:
   std::vector<VectorX> x_;
   std::vector<VectorU> u_;
   std::vector<Q> q_;
   std::vector<V> v_;
+  std::vector<VectorU> ku_;
+  std::vector<MatrixUX> kux_;
+  std::unique_ptr<DynamicModel> dynamic_model_;
+  std::unique_ptr<TransitionCost> transition_cost_;
+  std::unique_ptr<StateCost> final_cost_;
 };
+
 
 int main() {
   /*
@@ -290,10 +401,12 @@ int main() {
     q.d2f_du_dx = q.d2f_dx_dx;
     q.d2f_du_du[0][0] = 4;
     q.d2f_du_du[1][1] = 4;
-    std::optional<Solver::V> v = Solver::ComputeV(q);
-    if (v) {
-      v->df_dx.print();
-      v->d2f_dx_dx.print();
+    Solver::V v;
+    Solver::VectorU ku;
+    Solver::MatrixUX kux;    
+    if (Solver::ComputeKV(q, &ku, &kux, &v)) {
+      v.df_dx.print();
+      v.d2f_dx_dx.print();
     }
   }
   
@@ -317,6 +430,20 @@ int main() {
     q.d2f_du_du.print();
   }
   
+  Solver solver;
+  solver.dynamic_model_ = std::make_unique<MyDynamicModel>();
+  solver.transition_cost_ = std::make_unique<MyTransitionCost>();
+  solver.final_cost_ = std::make_unique<MyFinalCost>();
+  solver.x_.push_back(Solver::VectorX());
+  solver.u_.push_back(Solver::VectorU());
+  solver.u_.push_back(Solver::VectorU());
+  solver.Solve();
+  for (const auto& x : solver.x_) {
+    x.print();
+  }
+  for (const auto& u : solver.u_) {
+    u.print();
+  }
   return 0;
 }
 
